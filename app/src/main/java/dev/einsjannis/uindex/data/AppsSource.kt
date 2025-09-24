1e9e23460838339249c373b94086be3c08f8cfd2
Jannis Piekarek <einsjannis@gmail.com>
2025-09-24 15:06:31 +0200

    Fixed apps appearing multiple times

--- app/src/main/java/dev/einsjannis/uindex/data/AppsSource.kt
+++ app/src/main/java/dev/einsjannis/uindex/data/AppsSource.kt
@@ -4,14 +4,16 @@ import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import android.util.Log
 
 class AppsSource(private val packageManager: PackageManager) {
-    val apps: List<ApplicationInfo> get() = buildList {
+    val apps: List<ApplicationInfo> get() {
         val intent = Intent(Intent.ACTION_MAIN).apply {
             addCategory(Intent.CATEGORY_LAUNCHER)
         }
-        packageManager.queryIntentActivities(intent, 0)
-            .forEach { add(it.activityInfo.applicationInfo) }
+        return packageManager.queryIntentActivities(intent, 0)
+            .distinctBy { it.activityInfo.applicationInfo.packageName }
+            .map { it.activityInfo.applicationInfo }
     }
     fun getIcon(app: ApplicationInfo): Drawable? {
         return packageManager.getDrawable(app.packageName, app.icon, app)
